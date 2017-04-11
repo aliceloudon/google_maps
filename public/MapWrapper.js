d5b19e3ecfbd25262fbaa0f951f7cd5ea588debb
Alice Loudon <alice_loudon@icloud.com>
2017-04-11 20:15:57 +0100

    Add info window

--- public/MapWrapper.js
+++ public/MapWrapper.js
@@ -3,21 +3,36 @@ var MapWrapper = function(container, coords, zoom) {
 }
 
 MapWrapper.prototype = {
-  
-  addMarker: function(coords){
-    var marker = new google.maps.Marker({ position: coords, map: this.googleMap })
+
+  addMarker: function(coords, contentString){
+    
+    var marker = new google.maps.Marker({ 
+      position: coords, 
+      map: this.googleMap 
+    })
+
+    var infowindow = new google.maps.InfoWindow({ 
+      content: contentString 
+    })
+
+    marker.addListener('click', function() {
+      infowindow.open(this.googleMap, marker);
+    })
+
   },
+
 
   addClickEvent: function(){
     google.maps.event.addListener(this.googleMap, 'click', function(event){
-      // console.log(event)
-      // console.log(event.latLng.lat())
-      // var center = { lat: event.latLng.lat(), lng: event.latLng.lng()}
       this.addMarker(event.latLng)
     }.bind(this))
-    // Whenever you've got a callback inside a function, you need to be careful what THIS is.
-    // If you don't bind it, you are calling .addMarker on google.maps.event 
-    // but we want to call it on MapWrapper, so we do .bind(this)
-  }
+  },
+
 
 }
+
+
+// WHEN TO USE BIND...
+// Whenever you've got a callback inside a function, you need to be careful what THIS is.
+// If you don't bind it, you are calling .addMarker on google.maps.event 
+// but we want to call it on MapWrapper, so we do .bind(this)
--- public/app.js
+++ public/app.js
@@ -1,10 +1,12 @@
 var initialize = function() {
   var center = { lat: 55.938921, lng: -3.199422 }
+  var contentString = 'This is where I live'
   var container = document.getElementById('main-map')
   var mainMap = new MapWrapper(container, center, 15)
   
-  mainMap.addMarker(center)
+  mainMap.addMarker(center, contentString)
   mainMap.addClickEvent()
+
 }
 
 window.onload = initialize
